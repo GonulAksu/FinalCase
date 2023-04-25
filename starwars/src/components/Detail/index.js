9acb911fb3d20d238695aa97a0b3d671e1f6a43d
Gönül Aksu <aks766gnl@hotmail.com>
2023-04-25 19:49:00 +0300

    cursoricon

--- starwars/src/components/Detail/index.js
+++ starwars/src/components/Detail/index.js
@@ -5,31 +5,25 @@ import { Link } from 'react-router-dom';
 const Detail = ({ ship }) => {
     return (
         <Link  to={`ship/${ship.id}`} variant="link" >
-            <Card maxW='sm'>
-                <CardBody>
-                    <Stack mt='6' spacing='3'>
-                        <Heading size='md'>Living room Sofa</Heading>
-                        <Image
+            <Card className='detailcard' maxW='sm' boxShadow='dark-lg' p='6'>
+            <Image
                             src='https://picsum.photos/seed/picsum/100/100'
                             alt='Green double couch with wooden legs'
                             borderRadius='lg'
                         />
-                        <Text color='blue.600' fontSize='2xl'>
-                            {ship.name}
+                <CardBody>
+                    <Stack mt='4' spacing='3'>
+                        <Heading size='md'> {ship.name}</Heading>
+                        <Divider />
+                        <Text  align={"start"} color='blue.600' fontSize='lg'>
+                           Model: {ship.model}
+                        </Text>
+                        <Text  align={"start"} color='blue.600' fontSize='lg'>
+                        Hyperdrive Rating: {ship.hyperdrive_rating
+}
                         </Text>
                     </Stack>
                 </CardBody>
-                <Divider />
-                <CardFooter>
-                    <ButtonGroup spacing='2'>
-                        <Button variant='solid' colorScheme='blue'>
-                            Buy now
-                        </Button>
-                        <Button variant='ghost' colorScheme='blue'>
-                            Add to cart
-                        </Button>
-                    </ButtonGroup>
-                </CardFooter>
             </Card>
         </Link>
 
